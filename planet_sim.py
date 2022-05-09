bd1e703d8bb62abe22b44cdabd609d7296c21287
richard <richardinruntime@gmail.com>
2022-05-09 12:13:00 +0800

    Add original tutorial file

--- planet_sim.py
+++ planet_sim.py
@@ -6,10 +6,41 @@ WIDTH, HEIGHT = 800, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Planet Simulation")
 
+class Planet:
+    AU = 149.6e6 * 1000
+    G = 6.67428e-11
+    SCALE = 250 / AU    # 1AU = 100 pixel
+    TIMESTEP = 3600*24  # 1 day
+
+    def __init__(self, x, y, radius, color, mass):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        self.mass =  mass
+
+        self.orbit = []
+        self.sun = False
+        self.distance_to_sun = 0
+
+        self.x_vel = 0
+        self.y_vel = 0
+
+    def draw(self, win):
+        x = self.x * self.SCALE
+
+
+WHITE = (255, 255, 255)
+
 def main():
     run = True
+    clock = pygame.time.Clock()
 
     while run:
+        clock.tick(60)
+        WIN.fill(WHITE)
+        pygame.display.update()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
